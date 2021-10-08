90077290ed519ad818659ebcfb184d2ef58d0d78
anoy369 <adanoy369@gmail.com>
2021-10-08 07:17:45 +0600

    added firebase authentication and signin with google

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,14 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-export const auth = firebase.initializeApp({
+export const app = initializeApp({
   apiKey: "AIzaSyBP6u5AdO14UAApW3-oKyeXjxevV46gsms",
   authDomain: "kothon-2bd36.firebaseapp.com",
   projectId: "kothon-2bd36",
   storageBucket: "kothon-2bd36.appspot.com",
   messagingSenderId: "39390523118",
   appId: "1:39390523118:web:f5a3a7f534b11236f40dff",
-  measurementId: "G-HBEKD49HC7"
-}).auth();
+  measurementId: "G-HBEKD49HC7",
+});
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,42 @@
-import "firebase/app"
+import { signInWithRedirect } from "firebase/auth";
+import { auth, provider } from "../Firebase";
 
-// import { auth } from "../firebase";
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import GoogleIcon from '@mui/icons-material/Google';
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import GoogleIcon from "@mui/icons-material/Google";
 
 const login = () => {
-    return (
-        <Container>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center',justifyContent: "center", height: '100vh' }}>
-                <Typography variant="h2" component="h1" gutterBottom sx={{fontWeight: 500}}>
-                    Kothon
-                </Typography>   
-                <Button variant="contained" color="error"  startIcon={<GoogleIcon />}>Sign In With Google</Button>               
-            </Box>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <Typography
+          variant="h2"
+          component="h1"
+          gutterBottom
+          sx={{ fontWeight: 500 }}
+        >
+          Kothon
+        </Typography>
+        <Button
+          onClick={() => signInWithRedirect(auth, provider)}
+          variant="contained"
+          color="error"
+          startIcon={<GoogleIcon />}
+        >
+          Sign In With Google
+        </Button>
+      </Box>
+    </Container>
+  );
+};
 export default login;
