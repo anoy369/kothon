d9af9aafda32d976b22bbcfcdbdc67066c584833
anoy369 <adanoy369@gmail.com>
2021-10-21 05:38:44 +0600

    Chat UI

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -11,18 +11,23 @@ import {
 } from "firebase/firestore";
 import { auth, firestore } from "../Firebase";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import { Box } from "@mui/system";
+import { blue } from "@mui/material/colors";
+import { Button, TextField } from "@mui/material";
 
 const Chats = () => {
   const [messages, setMessages] = useState([]);
+  const chatBoxColor = blue[50];
 
   useEffect(() => {
     const messagesRef = collection(firestore, "messages");
     const queryData = query(messagesRef, orderBy("sendingTime"), limit(25));
     const unsubscribe = onSnapshot(queryData, (querySnapshot) => {
+      const messages = [];
       querySnapshot.forEach((doc) => {
-        setMessages([...messages, doc.data().text]);
-        // messages.push(doc.data().text);
+        messages.push(doc.data().text);
       });
+      setMessages([...messages]);
       // console.log("All TEXT: ", messages.join(", "));
     });
   }, []);
@@ -45,26 +50,55 @@ const Chats = () => {
   return (
     <div className="chatPage">
       <Nav />
-      <div>
-        {/* {messages &&
-          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)} */}
-        {console.log("messages", messages.length)}
-        {messages.map((msg) => (
-          <div>{messages}</div>
-        ))}
-      </div>
+      <Box sx={{ p: 1 }}>
+        <div>
+          {messages.map((msg) => (
+            <>
+              <Box
+                sx={{
+                  bgcolor: chatBoxColor,
+                  p: 1,
+                  mb: 1,
+                  display: "inline-flex",
+                  borderRadius: 1,
+                }}
+              >
+                {msg}
+              </Box>
+              <br />
+            </>
+          ))}
+        </div>
 
-      <form onSubmit={sendMessage}>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="say something nice"
-        />
-
-        <button type="submit" disabled={!formValue}>
-          🕊️
-        </button>
-      </form>
+        <form onSubmit={sendMessage}>
+          <Box sx={{ display: "flex" }}>
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
+              sx={{
+                flexGrow: 1,
+                pt: 0,
+                "& .MuiOutlinedInput-root": {
+                  borderRadius: "4px 0px 0px 4px",
+                },
+              }}
+              inputProps={{
+                sx: { py: 1 },
+              }}
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              disabled={!formValue}
+              sx={{ borderRadius: "0px 4px 4px 0px" }}
+            >
+              Send
+            </Button>
+          </Box>
+        </form>
+      </Box>
     </div>
   );
 };
